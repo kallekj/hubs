5f14bd2a7103c2dfd4ea2b5f1b80660210574579
AndreasH96 <andreashaggstrom96@gmail.com>
2020-07-07 15:19:59 +0200

    improved useability for video snapping

--- src/components/floaty-object.js
+++ src/components/floaty-object.js
@@ -19,13 +19,29 @@ AFRAME.registerComponent("floaty-object", {
     reduceAngularFloat: { default: false },
 
     // Velocity speed limit under which gravity will not be added if modifyGravityOnRelease is true
-    gravitySpeedLimit: { default: 1.85 } // Set to 0 to never apply gravity
+    gravitySpeedLimit: { default: 0 } // Set to 0 to never apply gravity
   },
 
   init() {
     this.onGrab = this.onGrab.bind(this);
     this.onRelease = this.onRelease.bind(this);
+    this.media_loaders = AFRAME.scenes[0].querySelectorAll("[media-loader]");
+    this.coloredObjects = [];
+    this.snapobjects = [];
+    this.currentTick = 0;
+    this.currentSnapTarget = 0;
+    var i = 0;
+    for (i = 0; i < this.media_loaders.length; i++) {
+      // If the object to snap onto has a 3D object
+      if (this.media_loaders[i].object3D != null) {
+        // Check if object is of the desired type
+        if (this.media_loaders[i].object3D.name.substring(0, 10).toLowerCase() == "snapobject") {
+          this.snapobjects.push(this.media_loaders[i]);
+        }
+      }
+    }
   },
+  // ----------------------- CUSTOM CODE --------------------------
   snap(toSnap, snapOn) {
     // Align rotation
     toSnap.el.object3D.setRotationFromQuaternion(snapOn.object3D.getWorldQuaternion()); //.rotation.copy(snapOn.object3D.);
@@ -36,6 +52,7 @@ AFRAME.registerComponent("floaty-object", {
     // Move slightly to avoid texture tearing
     toSnap.el.object3D.translateZ(0.002);
   },
+  // --------------------------------------------------------------
   tick() {
     if (!this.bodyHelper) {
       this.bodyHelper = this.el.components["body-helper"];
@@ -43,33 +60,111 @@ AFRAME.registerComponent("floaty-object", {
 
     const interaction = AFRAME.scenes[0].systems.interaction;
     const isHeld = interaction && interaction.isHeld(this.el);
+
+    // ----------------------Custom Code for highlighting snapping object where video will be snapped---------------
+    this.currentTick = this.currentTick + 1;
+    // Every 15th tick check if video object is nearby snap-object
+    if (this.currentTick % 15 == 0) {
+      // Check if floaty object is currently held by user and that it is of type media-video
+      if (isHeld && this.el.getAttribute("media-video") != null) {
+        // Check that there are any snap-objects in the environment
+        if (this.snapobjects.length > 0) {
+          // Get the media object, not necessary since it's just "this.el"
+          var mediaObject = this.el;
+          // Go through the snap-objects to find the closest
+          var closestObject = this.snapobjects[0];
+          for (let snapobject of this.snapobjects) {
+            if (
+              mediaObject.object3D.getWorldPosition().distanceTo(snapobject.object3D.getWorldPosition()) <
+              mediaObject.object3D.getWorldPosition().distanceTo(closestObject.object3D.getWorldPosition())
+            ) {
+              closestObject = snapobject;
+            }
+          }
+          // A multiplier depending on the size of the snap-object for better snapping range
+          const scaleMultiplier = (closestObject.object3D.scale.x * closestObject.object3D.scale.y) / 2;
+          // Check if close enough to snap
+          if (
+            this.el.object3D.getWorldPosition().distanceTo(closestObject.object3D.getWorldPosition()) <
+            0.4 + scaleMultiplier * 0.2
+          ) {
+            // Check if closest object already is colored
+            if (this.coloredObjects.indexOf(closestObject) < 0) {
+              // If not, empty list of colored objects, since there should be only one colored object
+              if (this.coloredObjects.length > 0) {
+                for (let oldObject of this.coloredObjects) {
+                  oldObject.object3DMap.mesh.material.opacity = 1;
+                  this.coloredObjects.splice(this.coloredObjects.indexOf(oldObject, 1));
+                }
+              }
+              // Mark closest object by changing its opacity
+              closestObject.object3DMap.mesh.material.opacity = 0.5;
+              this.coloredObjects.push(closestObject);
+            }
+          } else {
+            // If no object is close enough, then set objects to normal opacity
+            for (let coloredObject of this.coloredObjects) {
+              coloredObject.object3DMap.mesh.material.opacity = 1;
+              this.coloredObjects.splice(this.coloredObjects.indexOf(coloredObject, 1));
+            }
+          }
+        }
+      }
+    }
+    if (this.currentTick > 1000000) {
+      this.currentTick = 0;
+    }
+    // ---------------------------------------------------------------------------------------------------------------
     if (isHeld && !this.wasHeld) {
       this.onGrab();
     }
 
     if (this.wasHeld && !isHeld) {
-      this.onRelease();
-      // Custom code for snapping videos
+      //------------------------------- Custom code for snapping videos--------------------------
       // Check that the object is a video loader.
       if (this.el.getAttribute("media-video") != null) {
         // Load the objects which can be snapped on
         media_loaders = AFRAME.scenes[0].querySelectorAll("[media-loader]");
+        var snapobjects = [];
         var i = 0;
         for (i = 0; i < media_loaders.length; i++) {
           // If the object to snap onto has a 3D object
           if (media_loaders[i].object3D != null) {
             // Check if object is of the desired type
             if (media_loaders[i].object3D.name.substring(0, 10).toLowerCase() == "snapobject") {
-              // If close enough to an object
-              if (this.el.object3D.getWorldPosition().distanceTo(media_loaders[i].object3D.getWorldPosition()) < 0.5) {
-                // Snap onto it
-                this.snap(this, media_loaders[i]);
-                break;
-              }
+              snapobjects.push(media_loaders[i]);
+            }
+          }
+        }
+        // Check if there are any snap objects in the scene
+        if (snapobjects.length > 0) {
+          var mediaObject = this.el;
+          // Sort list of snap objects by distance to media objects
+          var sortedSnapObjects = snapobjects.sort(function(a, b) {
+            return (
+              mediaObject.object3D.getWorldPosition().distanceTo(a.object3D.getWorldPosition()) -
+              mediaObject.object3D.getWorldPosition().distanceTo(b.object3D.getWorldPosition())
+            );
+          });
+          // A multiplier depending on the size of the snap-object for better snapping range
+          const scaleMultiplier = (sortedSnapObjects[0].object3D.scale.x * sortedSnapObjects[0].object3D.scale.y) / 2;
+          // If the closest object is close enough
+          if (
+            this.el.object3D.getWorldPosition().distanceTo(sortedSnapObjects[0].object3D.getWorldPosition()) <
+            0.4 + scaleMultiplier * 0.2
+          ) {
+            // Snap onto it
+            this.snap(this, sortedSnapObjects[0]);
+            // Set the opacity of all snap objects to normal
+            for (let coloredObject of this.coloredObjects) {
+              coloredObject.object3DMap.mesh.material.opacity = 1;
+              this.coloredObjects.splice(this.coloredObjects.indexOf(coloredObject, 1));
             }
           }
         }
       }
+      this.onRelease();
+      //------------------------------------------------------------------------------------------
     }
 
     if (!isHeld && this._makeStaticWhenAtRest) {
@@ -91,26 +186,8 @@ AFRAME.registerComponent("floaty-object", {
         this._makeStaticWhenAtRest = false;
       }
     }
-    //-------------------CUSTOM------------------------
+
     this.wasHeld = isHeld;
-    // const userinput = AFRAME.scenes[0].systems.userinput;
-    // if (this.el.object3D.name.substring(0, 16) == "Interactive_Desk") {
-    //   var avatars = document.querySelectorAll("[networked-avatar]")
-    //   if (userinput.get("/actions/raiseNearestDesk")) {
-
-    //     if (this.el.object3D.getWorldPosition().y < 1.402) {
-    //       this.el.object3D.translateY(0.0007);
-    //       this.el.invisible_desk.object3D.translateY(0.0007);
-
-    //     }
-    //   } else if (userinput.get("/actions/lowerNearestDesk")) {
-    //     if (this.el.object3D.getWorldPosition().y > 0.905) {
-    //       this.el.object3D.translateY(-0.0007);
-    //       this.el.invisible_desk.object3D.translateY(-0.0007);
-    //     }
-    //   }
-    // }
-    //---------------------------------------------------
   },
 
   play() {
